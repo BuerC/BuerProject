db3f5139c30996123970281070b8d1da057cd7c0
Buer <wschenbuer@yahoo.com>
2017-12-26 17:18:35 +0100

    Commit message

--- workspace/lab4final/src/stack/EmptyStackException.java
+++
@@ -1,7 +0,0 @@
-package stack;
-public class EmptyStackException extends Exception{
-	public EmptyStackException()
-	{
-		
-	}
-}
--- workspace/lab5/src/lab5/IsEmpty.java
+++
@@ -1,5 +0,0 @@
-package lab5;
-
-public class IsEmpty extends Exception {
-
-}
